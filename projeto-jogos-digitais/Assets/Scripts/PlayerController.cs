1b21aea68d2dba26e8a510f43a1c829e9e2c818a
Thiago Ayres KImura <thiagoayreskimura@gmail.com>
2024-09-02 00:30:31 -0300

    Add tiro e animation
    Criação do tiro e animação do tiro apos clique do mouse ou pressionar espaço

--- projeto-jogos-digitais/Assets/Scripts/PlayerController.cs
+++ projeto-jogos-digitais/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] public GameObject meuTiro;
     [SerializeField] float velocidade = 5f;
     private Rigidbody2D meuRB;
     // Start is called before the first frame update
@@ -17,12 +19,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Pegando o input horizontal
-        var horizontal = Input.GetAxis("Horizontal") * velocidade;
+        var horizontal = Input.GetAxis("Horizontal");
         //Pegando o input vertical
-        var vertical = Input.GetAxis("Vertical") * velocidade;
+        var vertical = Input.GetAxis("Vertical");
         //Criando Vector2 para definir a velocidade do personagem
         Vector2 minhaVelocidade = new Vector2(horizontal, vertical);
+        //Normalizando a velocidade
+        minhaVelocidade.Normalize();
         //Passando a minha velocidade para meuRB
-        meuRB.velocity = minhaVelocidade;
+        meuRB.velocity = minhaVelocidade * velocidade;
+
+        //Atira com o botao1 do mouse ou espaco
+        if (Input.GetButtonDown("Fire1")){
+            Instantiate(meuTiro, transform.position, transform.rotation);
+        }
+            
     }
 }
---
+++ projeto-jogos-digitais/Assets/Scripts/TiroController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroController : MonoBehaviour
+{
+
+    private Rigidbody2D meuRB;
+    [SerializeField] private float vel = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Usando RigidBody2D
+        meuRB = GetComponent<Rigidbody2D>();
+
+        //Indo para cima
+        meuRB.velocity = new Vector2(0f, vel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
